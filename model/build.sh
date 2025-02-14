23f1441815fc9ef463ba2bb2ea257981ed70715e
Nafay <nafay2002rizwani@gmail.com>
2025-02-14 10:09:40 +0500

    LoRa models added

--- model/build.sh
+++ model/build.sh
@@ -53,7 +53,7 @@ download_huggingface 'https://civitai.com/api/download/models/1408658?type=Model
 
 download_huggingface 'https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors' "${TARGET_DIR}/checkpoints" "StableDiffusionXL.safetensors"
 
-download_huggingface 'https://huggingface.co/SG161222/RealVisXL_V4.0/resolve/main/RealVisXL_V4.0.safetensors' "${TARGET_DIR}/checkpoints" "RealVisXL_V4.0.safetensors"
+download_huggingface 'https://huggingface.co/SG161222/RealVisXL_V4.0/resolve/main/RealVisXL_V4.0.safetensors' "${TARGET_DIR}/checkpoints" "RealVisXL_V40.safetensors"
 
 download_huggingface 'https://civitai.com/api/download/models/133805?type=Model&format=SafeTensor' "${TARGET_DIR}/loras" "FantasyArtXL.safetensors"
 
